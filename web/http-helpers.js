a42af050c251dc14fced98ab5cdd4d411b7f5983
Student <student@MKS-SF-2.local>
2015-03-27 15:30:01 -0500

    initial work to pass first test

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -11,9 +11,9 @@ exports.headers = headers = {
 };
 
 exports.serveAssets = function(res, asset, callback) {
-  // Write some code here that helps serve up your static files!
-  // (Static files are things like html (yours or archived from others...),
-  // css, or anything that doesn't change often.)
+  var file = archive.paths.siteAssets + asset;
+  var readfile = fs.createReadStream(file);
+  readfile.pipe(res);
 };
 
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,14 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var request = require('http-request');
+var headerFile = require('./http-helpers.js');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if(req.method === 'GET' ){
+    var statusCode = 200;
+    headerFile.serveAssets(res,'/index.html');
+  }
+
+  // res.end(archive.paths.list);
 };
